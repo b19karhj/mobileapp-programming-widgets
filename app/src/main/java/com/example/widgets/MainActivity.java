47a60fac9a4d516181c21fa403f718b367997950
b19karhj <b19karhj@student.his.se>
2021-04-09 08:48:23 +0200

    Created Button widget and added it to javascript and added button to import

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b =findViewById(R.id.myNewBtn);
     }
 }
