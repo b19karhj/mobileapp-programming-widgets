aad6622c3198fd740b20872023c59ddc43a2a7af
b19karhj <b19karhj@student.his.se>
2021-04-09 09:14:42 +0200

    Added log event when we click on the button

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import android.os.Bundle;
@@ -13,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button b =findViewById(R.id.myNewBtn);
+          b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","We clicked on my button!");
+            }
+        });
     }
 }
